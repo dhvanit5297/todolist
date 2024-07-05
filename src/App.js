1664ec2e4b1babbe2d561ee034108523e4de42a2
Dhvanit <dhvanitkubavat5297@gmail.com>
2024-07-06 01:43:04 +0530

    3rd commit responsive,html viewport elememt importance

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import {useState } from "react";
 import "./App.css";
 import List from "./List";
 import { v4 as uuidv4 } from "uuid";
+import {toast } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [inputText, setInput] = useState("");
   const [todoList, setTodoList] = useState([]);
+  
+
+
 
   const handleInput = () => {
+
+    if(inputText.length===0){
+      toast.error('Input cannot be empty', {
+        position: "top-right",toastId:"error1",autoClose:2500
+        });
+        return;
+    }
+
     const newItem = {
       id: uuidv4(),
       data: inputText,
@@ -20,9 +34,10 @@ function App() {
   const newUpdate = (data) => {
     setTodoList(data);
   };
-
+  
   return (
     <>
+    <ToastContainer />
       <div className="App">
         <h1>To-Do-List</h1>
         <input
